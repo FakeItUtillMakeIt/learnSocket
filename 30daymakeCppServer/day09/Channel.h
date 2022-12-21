9579a7efca5b0677d1068543bbba59d0045c0989
JinLi <30925114+FakeItUtillMakeIt@users.noreply.github.com>
2022-12-21 12:00:50 +0800

    添加缓冲区类

---
+++ 30daymakeCppServer/day09/Channel.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <sys/epoll.h>
+#include <functional>
+
+class EventLoop;
+
+class Channel{
+	private:
+		EventLoop* loop;
+		int fd;
+		uint32_t events;
+		uint32_t revents;
+		bool inEpoll;
+
+		std::function<void()> callback;
+
+	public:
+		Channel(EventLoop *_loop,int _fd);
+		~Channel();
+
+		void handleEvent();
+		void enableReading();
+
+		int getfd();
+		uint32_t getEvents();
+		uint32_t getRevents();
+		bool getInEpoll();
+		void setInEpoll();
+
+		void setRevents(uint32_t);
+
+		void setCallback(std::function<void()>);
+};
---
+++ 30daymakeCppServer/day09/Connection.cpp
@@ -0,0 +1,56 @@
+#include "Connection.h"
+#include "Socket.h"
+#include "Channel.h"
+#include "Buffer.h"
+#include <unistd.h>
+#include <string.h>
+
+#define READ_BUFFER 1024
+
+Connection::Connection(EventLoop *_loop,Socket *_sock):loop(_loop),sock(_sock),channel(NULL),readBuffer(NULL),inBuffer(new std::string()){
+	channel=new Channel(loop,sock->getfd());
+	std::function<void()> cb=std::bind(&Connection::echo,this,sock->getfd());
+	channel->setCallback(cb);
+	channel->enableReading();
+
+	readBuffer=new Buffer();
+}
+
+Connection::~Connection(){
+	delete channel;
+	delete sock;
+	delete readBuffer;
+}
+
+void Connection::echo(int sockfd) {
+	char buf[READ_BUFFER];
+	while(true){
+		bzero(buf,READ_BUFFER);
+		ssize_t bytes_read=read(sockfd,buf,READ_BUFFER);
+		if(bytes_read>0){
+			//printf("message from fd:%d,data:%s\n",sockfd,buf);
+			//write(sockfd,buf,READ_BUFFER);
+			readBuffer->append(buf,bytes_read);
+		}
+		else if(bytes_read==-1 && errno==EINTR){
+			printf("continue reading\n");
+			continue;
+		}
+		else if(bytes_read==-1 &&((errno==EAGAIN)||(errno==EWOULDBLOCK))){
+			printf("finish reading once,errno:%d\n",errno);
+			printf("message from client fd:%d,data:%s\n",sockfd,readBuffer->c_str());
+			write(sockfd,readBuffer->c_str(),readBuffer->size());
+			readBuffer->clear();
+			break;
+		}
+		else if(bytes_read==0){
+			printf("EOF,client fd %d disconnected\n",sockfd);
+			deleteConnectionCallback(sock);//guan bi sockfd will remove fd from epoll trees
+			break;
+		}
+	}
+}
+
+void Connection::setDeleteConnectionCallback(std::function<void(Socket*)> _cb){
+	deleteConnectionCallback=_cb;
+}
